fadaa7c7c6df29495441add49a51902e40b157f2
zaidan126 <cuikomah21@gmail.com>
2023-05-25 20:29:01 +0700

    first project

---
+++ first project.py
@@ -0,0 +1,5 @@
+print("this is my first project in python")
+text="helllllllllllllllllllllllllooooooooooooooooooooooooo"
+_number1 =10000000000000
+_number2 =3.14
+print(type(text),type(_number1),type(_number2))
