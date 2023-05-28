2e6742d8ec93f843f12ea06f4e9198747ae3ca71
zaidan126 <cuikomah21@gmail.com>
2023-05-28 14:05:09 +0700

    traveling done.py

--- traveling.py
+++ traveling.py
@@ -1,17 +1,48 @@
 country= input("pick a country where you want to travel: ")
 if country== "America":
-    city= input("pick a city:")
+    city= input("pick a city: ")
     if city== "New york":
         print("welcome to new york")
-        hotel= input("pick a hotel:")
+        hotel= input("pick a hotel: ")
         if hotel== "insignia":
             print("welcome to city hotel")
         elif hotel== "luxe studio":
             print("welcome to our villa")
     elif city== "los angeles":
         print("welcome to los angeles")
+        hotel= input("pick a hotel: ")
+        if hotel== "double tree":
+            print("welcome to our city hotel")
 elif country == "Russia":
-    pass
+    city= input("pick a city:")
+    if city== "Moscow":
+        print("welcome to Moscow")
+        hotel= input("pick a hotel:")
+        if hotel== "four season hotel":
+            print("welcome to city hotel")
+        elif hotel== "Lesnoy home":
+            print("welcome to our villa")
+    elif city== "Stalingrad":
+            print("welcome to stalingrad")
+    hotel= input("pick a hotel: ")
+    if hotel== "volgograd":
+            print("welcome to volgograd")
+         
 elif country == "Uk":
-    pass
+    city= input("pick a city: ")
+    if city== "London":
+        print("welcome to London")
+        hotel= input("pick a hotel: ")
+        if hotel== "the tower hotel":
+            print("welcome to our city hotel")
+        elif hotel== "villa collective":
+            print("welcome to our villa")
+    elif city== "Birmingham":
+        print("welcome to Birmingham")
+        hotel= input("pick a hotel: ")
+    if hotel== "hyatt regency":
+        print("welcome to our city hotel")
+
+
+       
 else: print("im sorry the country not detected")
