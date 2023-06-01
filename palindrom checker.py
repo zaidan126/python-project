4283aa1d82f98aa225aa4d663bfb6844bd7be4dd
zaidan126 <cuikomah21@gmail.com>
2023-06-01 20:32:55 +0700

    palindrom checker

---
+++ palindrom checker.py
@@ -0,0 +1,14 @@
+word= input("put a word or sentence to check: ")
+reversed=word
+# print (word,reversed)
+# for i in range(len(reversed)):
+#     print(reversed[i])
+temp=""
+for i in range(len(reversed),0,-1):
+    temp+=(reversed[i-1])
+print(temp)
+reversed=temp
+if word==reversed:
+    print("the word is palindrom")
+else:
+    print("the word is not palindrom")
