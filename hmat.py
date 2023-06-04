69d0c7e94bc2aa5e8826ce7d46af666fa1a42457
zaidan126 <cuikomah21@gmail.com>
2023-06-04 12:27:27 +0700

    explore math modules

---
+++ hmat.py
@@ -0,0 +1,10 @@
+# import math as m
+from math import *
+print(sqrt(2))
+print(pow(2,100))
+print(ceil(4.30))
+print(floor(4.30))
+print(round(4.9))
+print(pi)
+print(fabs(100-179))
+print(factorial(50))
---
+++ mainly geometric calculator.py
@@ -0,0 +1,4 @@
+import math as m
+print("the area of circle with radius of 3.72 in meter is:",m.pi*m.pow(3.72,2))
+print("the volume of sphere with radius of 3.72 in meter is:", (4/3)*m.pi*m.pow(3.72,3))
+print("the volume of cylinder with radius of 3.72 and height of 5.17 in meter is:",m.pi*m.pow(3.72,2)*5.17)
