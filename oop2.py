a8a2af84a86c98fe0e71e89d3f2c541852039510
zaidan126 <cuikomah21@gmail.com>
2023-07-02 12:34:26 +0700

    monster fighting

--- oop2.py
+++ oop2.py
@@ -3,9 +3,38 @@ class Character:
         self.name=name
         self.HP=HP
         self.MP=MP
-    def attack(self,character):
-        character.HP-=3
-        print(self.name,"attack",character.name)
-        print(character.name,"\'s HP:",character.HP)
+    def attack(self,enemy):
+        enemy.HP-=3
+        print(self.name,"attack",enemy.name)
+    def showstatus(self):
+        if self.HP>0:
+            print(self.name,"\'s HP:",self.HP)
+            print(self.name,"\'s MP:",self.MP)
+        else:
+            print(self.name,"died")
 class Mage(Character):
-    def plasma_beam(self,)
+    def plasma_beam(self,enemy):
+        if self.MP>=15:
+            enemy.HP-=21
+            self.MP-=15
+            print(self.name,"use plasma beam to",enemy.name)
+        else:
+            print(self.name,"not enough MP")
+    def plasma_heal(self,character):
+        if self.MP>=37:
+            character.HP+=50
+            self.MP-=37
+            print(self.name,"use plasma heal to",character.name)
+        else:
+            print(self.name,"not enough MP")
+class Slime(Character):
+    def attack(self, enemy):
+        enemy.HP-=20
+        print(self.name,"slime quake",enemy.name)
+    def Slime_ball(self,enemy):
+        if self.MP>=14:  
+            enemy.HP-=23
+            self.MP-=14
+            print(self.name,"use slime ball to",enemy.name)
+        else:
+            print(self.name,"not enough MP")
--- test3.py
+++ test3.py
@@ -1,4 +1,16 @@
 from oop2 import*
-player=Character("zaidan",120,310)
-monster=Character("slime",50,71)
+player=Mage("zaidan",120,310)
+monster=Slime("slime",50,71)
 player.attack(monster)
+monster.showstatus()
+player.plasma_beam(monster)
+player.showstatus()
+player.plasma_heal(player)
+player.showstatus()
+for i in range(7):
+    monster.Slime_ball(player)
+monster.showstatus()
+player.showstatus()
+for i in range(7):
+    player.plasma_beam(monster)
+    monster.showstatus()
